dedbaa892fbb0f46a82dfed57998911e31d7207a
ian3004 <135764441+ian3004@users.noreply.github.com>
2023-06-06 20:18:00 -0300

    Entrada de dados do usuario

--- EXEMPLO-GIT/LivrariaApplication.java
+++ EXEMPLO-GIT/LivrariaApplication.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class LivrariaApplication {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite sua idade:");
+        int idade = sc.nextInt();
+
+        if (idade>18){
+            System.out.println("é maior de idade");
+        }else
+        System.out.println("Menor de idade");
         
-    }
-    
+        
+        
+       
+        sc.close();
+        
+        
+
+       }
+
+
+
 }
